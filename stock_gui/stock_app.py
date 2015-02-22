cf7cee04e40d6e3f9b8721408f52e8f691a69644
Yi Zhang <zhangyiant@hotmail.com>
2015-02-22 09:30:37 +0800

    Added cash related menu.

--- stock_gui/stock_app.py
+++ stock_gui/stock_app.py
@@ -1,16 +1,28 @@
 import logging
 from tkinter import *
 
+def donothing():
+    return
+
 class StockApp:
     def __init__(self):
         self.root = Tk()
         self.init_stock_cash_menu()
     
     def init_stock_cash_menu(self):
-        self.cash_menu = Menu(self.root)
-        self.cash_menu.add_command(label="Hello!", command=self.hello)
-        self.cash_menu.add_command(label="Quit!", command=self.root.quit)
-        self.root["menu"] = self.cash_menu
+        self.menu_bar = Menu(self.root)
+        self.cash_menu = Menu(self.menu_bar)
+        self.other_menu = Menu(self.menu_bar)
+
+        self.cash_menu.add_command(label="New cash pool...", command=donothing)
+        self.cash_menu.add_command(label="Delete cash pool...", command=donothing)
+        self.cash_menu.add_command(label="Update cash pool...", command=donothing)
+        self.menu_bar.add_cascade(label="Cash", menu=self.cash_menu)
+
+        self.other_menu.add_command(label="new quit!", command=self.root.quit)
+        self.menu_bar.add_cascade(label="other", menu=self.other_menu)
+
+        self.root["menu"] = self.menu_bar
 
     def hello(self):
         print("Hello!")
