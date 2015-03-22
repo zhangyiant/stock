c3e53cc16b0e62be12a3d6ef61ceaebf01769394
Yi Zhang <zhangyiant@hotmail.com>
2015-03-22 08:05:47 +0800

    Added padx and pady for diaglog frame.

--- stock_gui/new_cash_pool_dialog.py
+++ stock_gui/new_cash_pool_dialog.py
@@ -8,10 +8,24 @@ class NewCashPoolFrame(Frame):
         self.init_widget()
 
     def init_widget(self):
+        self.columnconfigure(0, weight=3)
+        self.columnconfigure(1, weight=4)
+        self.columnconfigure(2, weight=5)
+        self.rowconfigure(0, weight=2)
+        self.rowconfigure(1, weight=3)
+        self.rowconfigure(2, weight=3)
         self.button = Button(self)
         self.button["text"] = "This is a test"
         self.button["command"] = self.hello
-        self.button.pack()
+        self.button.grid(row=1, column=1)
+
+        self.button2 = Button(self)
+        self.button2["text"] = "Button2"
+        self.button2.grid(row=0, column=0)
+
+        self.button3 = Button(self)
+        self.button3["text"] = "Button3"
+        self.button3.grid(row=2, column=2)
 
     def get_result(self):
         return 1000
@@ -28,7 +42,7 @@ class NewCashPoolDialog():
     def open(self):
         self.root = Toplevel()
         self.frame = NewCashPoolFrame(self.root)
-        self.frame.pack()
+        self.frame.grid(padx = 10, pady = 10)
         self.root.transient(self.master)
         self.root.grab_set()
         self.root.wait_window(self.root)
