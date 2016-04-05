58412450e042f0fba80d392ce2e4954c026f9826
Yi Zhang <zhangyiant@hotmail.com>
2016-04-05 21:23:54 +0800

    make sure the test can be run successfully.

--- test/test_stock_db_connection.py
+++ test/test_stock_db_connection.py
@@ -1,5 +1,7 @@
 import logging
 import unittest
+import configparser
+import stock_db
 from stock_holding_algorithm.simple_algorithm import SimpleAlgorithm
 from stock_db.db_connection import StockDbConnection
 from stock_db.db_stock import StockInfoTable
@@ -8,26 +10,31 @@ from stock_db.db_stock import StockCash
 from stock_db.db_stock import StockTransaction
 from stock_db.db_stock import StockTransactionTable
 from stock_db.db_utility import reset_table
+from stock_db.db_connection import get_default_db_connection
 from datetime import date
 
 class StockDbConnectionTest(unittest.TestCase):
     def setUp(self):
+        config = configparser.ConfigParser()
+        config.read("gtja_trade.ini", encoding="utf-8")
+        connection_string = config['Database'].get('connection')
+        stock_db.db_connection.default_connection_string = connection_string
         return
  
     def test_reset_table(self):
         # test
         logging.info("new StockDbConnection")
-        stock_db_connection = StockDbConnection('sqlite:///%s'  % "example.db")
+        stock_db_connection = get_default_db_connection()
         logging.info("reset table")
         reset_table(stock_db_connection)
 
         stock_info_table = StockInfoTable(stock_db_connection)
         stock_info_list = stock_info_table.get_all_stock_info()
-        self.assertEqual(stock_info_list, [])
-
+        self.assertEqual(len(stock_info_list), 7)
+        return
  
     def test_stock_cash_sanity(self):
-        stock_db_connection = StockDbConnection("example.db")
+        stock_db_connection = get_default_db_connection()
         reset_table(stock_db_connection)
         stock_cash_table = StockCashTable(stock_db_connection)
         stock_cash = StockCash("601398", 1000)
@@ -39,25 +46,25 @@ class StockDbConnectionTest(unittest.TestCase):
         self.assertEqual(stock_cash.get_amount(), 1000)
 
         # test update stock cash
-        stock_cash.set_amount(123456.566)
+        stock_cash.set_amount(23.456)
         stock_cash_table.update_stock_cash(stock_cash)
         stock_cash = stock_cash_table.get_stock_cash_by_symbol("601398")
-        self.assertEqual(stock_cash.get_amount(), 123456.566)
+        self.assertEqual(stock_cash.get_amount(), 23.456)
 
         # test an unavailable line
-        stock_cash = stock_cash_table.get_stock_cash_by_symbol("601390")
+        stock_cash = stock_cash_table.get_stock_cash_by_symbol("XXXXXX")
         self.assertEqual(stock_cash, None)
 
         # insert a new line, and test get_all function
-        stock_cash = StockCash("601390", 5000)
+        stock_cash = StockCash("601857", 5000)
         stock_cash_table.add_stock_cash(stock_cash)
         stock_cash_list = stock_cash_table.get_all_stock_cash()
         self.assertEqual(len(stock_cash_list), 2)
         stock_cash = stock_cash_list[0]
         self.assertEqual(stock_cash.get_symbol(), "601398")
-        self.assertEqual(stock_cash.get_amount(), 123456.566)
+        self.assertEqual(stock_cash.get_amount(), 23.456)
         stock_cash = stock_cash_list[1]
-        self.assertEqual(stock_cash.get_symbol(), "601390")
+        self.assertEqual(stock_cash.get_symbol(), "601857")
         self.assertEqual(stock_cash.get_amount(), 5000)
 
         # delete a line
@@ -67,7 +74,7 @@ class StockDbConnectionTest(unittest.TestCase):
         self.assertEqual(stock_cash, None)
 
     def test_stock_transaction_sanity(self):
-        stock_db_connection = StockDbConnection("example.db")
+        stock_db_connection = get_default_db_connection()
         reset_table(stock_db_connection)
         stock_transaction_table = StockTransactionTable(stock_db_connection)
         stock_transaction = StockTransaction()
