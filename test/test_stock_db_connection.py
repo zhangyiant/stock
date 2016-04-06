bd6660b71420f777b50bc9e81470e594cc27acf9
Yi Zhang <peter-yi.zhang@autodesk.com>
2016-04-06 12:16:20 +0800

    Update the config file to stock.ini in test file.

--- test/test_stock_db_connection.py
+++ test/test_stock_db_connection.py
@@ -16,7 +16,7 @@ from datetime import date
 class StockDbConnectionTest(unittest.TestCase):
     def setUp(self):
         config = configparser.ConfigParser()
-        config.read("gtja_trade.ini", encoding="utf-8")
+        config.read("stock.ini", encoding="utf-8")
         connection_string = config['Database'].get('connection')
         stock_db.db_connection.default_connection_string = connection_string
         return
