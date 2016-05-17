64cfa5ffafa2bb159f42e0e18e460c2534120aba
Yi Zhang <zhangyiant@hotmail.com>
2016-05-17 20:25:37 +0800

    Update the format issue.

--- stock_db/db_utility.py
+++ stock_db/db_utility.py
@@ -1,22 +1,26 @@
+'''
+    db_utility module
+'''
 import logging
 import csv
 
-from stock_db.db_connection import StockDbConnection
 from stock_db.db_connection import get_default_db_connection
 from stock_db.db_stock import StockInfoTable, StockInfo
 import stock_db.db_stock
 
-logger = logging.getLogger(__name__ + ".StockDbUtility")
+LOGGER = logging.getLogger(__name__ + ".StockDbUtility")
 
-def import_stock_info(conn = None):
-
+def import_stock_info(conn=None):
+    '''
+        import_stock_info
+    '''
     if conn is None:
         stock_info_table = StockInfoTable()
     else:
         stock_info_table = StockInfoTable(conn)
-        
-    with open("stock_info.csv", newline="", encoding="utf-8") as f:
-        reader = csv.reader(f)
+
+    with open("stock_info.csv", newline="", encoding="utf-8") as file_handler:
+        reader = csv.reader(file_handler)
         for row in reader:
             stock_symbol = row[0]
             stock_name = row[1]
@@ -33,38 +37,43 @@ def import_stock_info(conn = None):
     return
 
 def reset_table(conn = None):
-    
+    '''
+        reset_table
+    '''
     if conn is None:
         db_connection = get_default_db_connection()
     else:
         db_connection = conn
-        
-    Base = stock_db.db_stock.Base
-  
+
+    base = stock_db.db_stock.Base
+
     engine = db_connection.get_engine()
-    
-    Base.metadata.drop_all(engine)
-    Base.metadata.create_all(engine)
+
+    base.metadata.drop_all(engine)
+    base.metadata.create_all(engine)
 
     # import stock info from csv file
     import_stock_info()
 
     return
 
-def recreate_db(conn = None):
+def recreate_db(conn=None):
+    '''
+        recreate_db
+    '''
     if conn is None:
         db_connection = get_default_db_connection()
     else:
         db_connection = conn
 
-    Base = stock_db.db_stock.Base
+    base = stock_db.db_stock.Base
 
     engine = db_connection.get_engine()
 
-    Base.metadata.create_all(engine)
+    base.metadata.create_all(engine)
 
     return
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
     recreate_db()
